7faf040886c3863269f65fab48398ab77f354d73
Anamile23 <amacuna@soy.sena.edu.co>
2025-08-15 15:24:01 -0500

    ok

--- main.py
+++ main.py
@@ -55,8 +55,9 @@ def startup_event():
 
 # Ejecución local o en Render
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    port = int(os.environ.get("PORT", 10000))  # Render usa el puerto 10000
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
 
 
