77924a917b40d3dae7ccc842aae6ddd15a5bc5fb
Anamile23 <amacuna@soy.sena.edu.co>
2025-08-25 19:34:52 -0500

    ok

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
@@ -53,10 +52,73 @@ def startup_event():
     except Exception as e:
         print(f"⚠️ Error al inicializar Firebase: {e}")
 
- #  Bloque final para despliegue en Render
+
+# 👉 Bloque final corregido para Render
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    # Render ya pasa el puerto en la variable de entorno $PORT
+    # No hace falta añadir host ni port manualmente
+    uvicorn.run("main:app")
+
+
+
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# import os
+# import uvicorn
+# from dotenv import load_dotenv
+# from firebase import init_firebase, get_firestore
+
+# from app.routes.neonato_routes import router as neonato_router
+# from app.routes.madre_routes import router as madre_router
+# from app.routes.llanto_routes import router as llanto_router
+
+# load_dotenv()
+
+# app = FastAPI()
+
+# # CORS
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# # Rutas
+# app.include_router(neonato_router)
+# app.include_router(madre_router)
+# app.include_router(llanto_router)
+
+# @app.get("/")
+# async def read_root():
+#     return {"message": "Servidor funcionando correctamente"}
+
+# @app.get("/ping")
+# def ping():
+#     return {"status": "ok"}
+
+# @app.get("/health")
+# def health_check():
+#     try:
+#         db = get_firestore()
+#         docs = db.collections()  # 👈 Forzar consulta
+#         return {"firebase": "ok"}
+#     except Exception as e:
+#         return {"firebase": "error", "detail": str(e)}
+
+# @app.on_event("startup")
+# def startup_event():
+#     try:
+#         init_firebase()
+#         print("✅ Firebase inicializado correctamente")
+#     except Exception as e:
+#         print(f"⚠️ Error al inicializar Firebase: {e}")
+
+#  #  Bloque final para despliegue en Render
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8000))
+#     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
     
 
 
